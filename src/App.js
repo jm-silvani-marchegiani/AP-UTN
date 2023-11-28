16f8c08e89d0be2ba2ff2b7de44a61d9ce8caca2
FacundoBraga <facundobraga@hotmail.com>
2023-11-28 02:52:23 -0300

    Cambios en Navbar.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
