75f0faded6473f6f16c19ef77f9cd4c8315136fe
FacundoBraga <facundobraga@hotmail.com>
2023-11-28 16:32:35 -0300

    Avances en estilados.

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,13 +1,22 @@
 import React from "react";
-import { Button } from "@mui/material";
-import DeleteIcon from "@mui/icons-material/Delete";
+import { Box, Grid, IconButton, Typography } from "@mui/material";
+import { ReactComponent as DeleteButton } from "../assets/button-delete.svg";
+import styled from "@emotion/styled";
+
+const StyledDeleteButton = styled(DeleteButton)({
+  "&:hover": {
+    "& rect": {
+      fill: "#23221F",
+    },
+  },
+});
 
 const TaskItem = ({ task, setTasks, tasks }) => {
   const handleDelete = (id) => {
     let result = tasks.filter((element) => element.id !== id);
     setTasks(result);
   };
-
+  // eslint-disable-next-line
   const toggleTaskStatus = (id) => {
     let result = tasks.map((element) => {
       if (element.id === id) {
@@ -23,37 +32,68 @@ const TaskItem = ({ task, setTasks, tasks }) => {
   };
 
   return (
-    <div
-      style={{
+    <Box
+      sx={{
         display: "flex",
-        justifyContent: "space-evenly",
-        border: "1px solid black",
+        width: "100%",
+        minWidth: { xs: "24rem", sm: "33rem", md: "48rem" },
+        marginTop: "33px",
       }}
     >
-      <div style={{ width: "50%" }}>
-        <h4
-          style={{ textDecoration: task.completed ? "line-through" : "none" }}
+      <Grid
+        container
+        sx={{
+          border: "2px solid #A35709",
+          borderRadius: "8px",
+          marginBottom: "16px",
+          maxWidth: "45rem",
+          marginRight: { lg: "3rem", xs: "0" },
+          ":hover": { backgroundColor: "#2B2A27" },
+        }}
+      >
+        <Grid
+          container
+          item
+          xs={10}
+          sx={{
+            display: "grid",
+            wordBreak: "break-word",
+            padding: "16px",
+          }}
         >
-          {task.title}
-        </h4>
-        <p style={{ textDecoration: task.completed ? "line-through" : "none" }}>
-          {task.description}
-        </p>
-      </div>
-      <div style={{ width: "30%" }}>
-        <Button
-          onClick={() => handleDelete(task.id)}
-          variant="outlined"
-          type="button"
-          startIcon={<DeleteIcon />}
-        >
-          Delete
-        </Button>
-        <Button onClick={() => toggleTaskStatus(task.id)} variant="outlined">
-          {!task.completed ? "Completar Tarea" : "Marcar como Pendiente"}
-        </Button>
-      </div>
-    </div>
+          <Typography
+            sx={{
+              textDecoration: task.completed ? "line-through" : "none",
+              fontSize: "22px",
+              color: "#F0E3CA",
+              fontWeight: "bold",
+            }}
+            gutterBottom
+          >
+            {task.title}
+          </Typography>
+          <Typography
+            sx={{
+              textDecoration: task.completed ? "line-through" : "none",
+              fontSize: "14px",
+              color: "#F0E3CA",
+            }}
+            gutterBottom
+          >
+            {task.description}
+          </Typography>
+        </Grid>
+
+        <Grid container item xs={2} sx={{ display: "grid" }}>
+          <IconButton variant="outlined" type="button" disableRipple>
+            <StyledDeleteButton onClick={() => handleDelete(task.id)} />
+          </IconButton>
+          {/* <Button onClick={() => toggleTaskStatus(task.id)} variant="outlined">
+            {!task.completed ? "Completar Tarea" : "Marcar como Pendiente"}
+          </Button> */}
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
