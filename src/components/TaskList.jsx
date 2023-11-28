ae20d007d94c8f734e7e9fb720591193adb9bdb9
FacundoBraga <facundobraga@hotmail.com>
2023-11-28 00:45:38 -0300

    Boton de eliminar tarea y Completar tarea añadido.

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 import TaskItem from "./TaskItem";
 
-const TaskList = ({ tasks }) => {
+const TaskList = ({ tasks, setTasks }) => {
   return (
     <>
       <h2>Lista de tareas</h2>
-      {tasks.map((task, index) => {
-        return <TaskItem key={index} task={task} />;
-      })}
+      {tasks
+        .slice()
+        .sort((a, b) => b.createdAt - a.createdAt)
+        .map((task, index) => {
+          return (
+            <TaskItem
+              setTasks={setTasks}
+              tasks={tasks}
+              key={index}
+              task={task}
+            />
+          );
+        })}
     </>
   );
 };
